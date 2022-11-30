928bef0b8c1fb9c5104a695119b7f35005d5bb62
Amanda Stjerna <amanda.stjerna@it.uu.se>
2022-11-30 13:39:26 +0100

    AutomatonBuilder: use more efficient BW reachability
    Note: reachability computation is now generalised.
    
    Closes: #42

--- bin/experiments-capheus.sh
+++ bin/experiments-capheus.sh
@@ -12,4 +12,4 @@ parallel -j$NR_THREADS --header : \
   --eta --results $CURRENT_VERSION.capheus java -jar -Xmx${RAM_ALLOC} $JARFILE \
   solve-satisfy --timeout 60000 \
   --backend {backend} {chunk} \
-  ::: backend nuxmv lazy ::: chunk chunks/chunk-*
+  ::: backend baseline nuxmv lazy ::: chunk chunks/chunk-*
