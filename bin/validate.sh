0aa27767bbb5e911a1a8121a6ed2abc332067f25
Amanda Stjerna <amanda.stjerna@it.uu.se>
2022-12-08 13:15:04 +0100

    Add runner script for validation experiments

---
+++ bin/validate.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+JARFILE=./target/scala-2.13/uuverifiers/catra-assembly-0.1.0-SNAPSHOT.jar
+CURRENT_VERSION=$(git rev-parse --short HEAD)
+RAM_ALLOC=4g
+NR_THREADS=10
+
+git reset --hard
+git pull
+sbt assembly
+
+parallel -j$NR_THREADS --header : \
+  --eta --results $CURRENT_VERSION.validation java -jar -Xmx${RAM_ALLOC} $JARFILE \
+  solve-satisfy --timeout 60000 \
+  --backend lazy --cross-validate {chunk} \
+  ::: chunk chunks/chunk-*
