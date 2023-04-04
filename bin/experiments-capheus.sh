fca705b867195ee7558ab30cab8f07cf2932c273
Amanda Stjerna <amanda.stjerna@it.uu.se>
2023-04-04 11:13:22 +0200

    Increase the timeout of experiments

--- bin/experiments-capheus.sh
+++ bin/experiments-capheus.sh
@@ -3,6 +3,7 @@ JARFILE=./target/scala-2.13/uuverifiers/catra-assembly-0.1.0-SNAPSHOT.jar
 CURRENT_VERSION=$(git rev-parse --short HEAD)
 RAM_ALLOC=10g
 NR_THREADS=8
+TIMEOUT=120000
 
 #git reset --hard
 #git pull
@@ -10,6 +11,6 @@ sbt assembly
 
 parallel -j$NR_THREADS --header : \
   --eta --results $CURRENT_VERSION.capheus java -jar -Xmx${RAM_ALLOC} $JARFILE \
-  solve-satisfy --timeout 60000 \
+  solve-satisfy --timeout ${TIMEOUT} \
   --backend {backend} {chunk} \
   ::: backend baseline nuxmv lazy ::: chunk chunks/chunk-*
