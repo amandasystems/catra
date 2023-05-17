e6c8e144b5e3d1ecb7c9c50faa5ea9ef268c9065
Amanda Stjerna <amanda.stjerna@it.uu.se>
2023-05-17 15:29:39 +0200

    Nicer runner scripts

--- bin/experiments.sh
+++ bin/experiments.sh
@@ -3,8 +3,11 @@
 CURRENT_VERSION=$(sbt -Dsbt.supershell=false -error "print benchmark/version")
 RESTART_AFTER=10
 LOGFILE="logs/catra-${CURRENT_VERSION}.experiments.log"
+JARFILE="benchmark/target/scala-2.13/catra-benchmark-assembly-${CURRENT_VERSION}.jar"
+
+mkdir -p logs
 
 echo "${CURRENT_VERSION}: Executing experiments in parallel, logging to ${LOGFILE}"
 find $@ -type f | xargs -n $RESTART_AFTER -- \
-  java -XX:MaxRAMPercentage=90.0 -jar runner/target/scala-2.13/catra-benchmark-assembly-"${CURRENT_VERSION}".jar \
-  | tee "$LOGFILE"
+  java -XX:MaxRAMPercentage=90.0 -jar "$JARFILE" | tee "$LOGFILE"
+echo "Done with all experiments!" >> $LOGFILE
--- bin/validate.sh
+++ bin/validate.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 CURRENT_VERSION=$(sbt -Dsbt.supershell=false -error "print validator/version")
-
+mkdir -p logs
 sbt "validator/run $@" | tee logs/${CURRENT_VERSION}.validation.log
