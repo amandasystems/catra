2edc512c5d78dda7b1308fec707abd56e1e30e73
Amanda Stjerna <amanda.stjerna@it.uu.se>
2023-05-24 10:36:45 +0200

    Add safety valve

--- bin/experiments.sh
+++ bin/experiments.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -x
 CURRENT_VERSION=$(sbt -Dsbt.supershell=false -error "print benchmark/version")
 RESTART_AFTER=10
 LOGFILE="logs/catra-${CURRENT_VERSION}.experiments.log"
@@ -8,6 +8,7 @@ JARFILE="benchmark/target/scala-2.13/catra-benchmark-assembly-${CURRENT_VERSION}
 mkdir -p logs
 
 echo "${CURRENT_VERSION}: Executing experiments in parallel, logging to ${LOGFILE}"
-find $@ -type f | xargs -n $RESTART_AFTER -- \
-  java -XX:MaxRAMPercentage=90.0 -jar "$JARFILE" | tee "$LOGFILE"
-echo "Done with all experiments!" >> $LOGFILE
+find "$@" -type f | xargs -n $RESTART_AFTER -- \
+  timeout 1h java -XX:MaxRAMPercentage=90.0 -jar "$JARFILE" | tee "$LOGFILE"
+
+echo "Done with all experiments!" >> "$LOGFILE"
