905c75fb1c0ef84b09bdc922d86ac7eb605399af
Amanda Stjerna <amanda.stjerna@it.uu.se>
2023-06-05 16:42:30 +0200

    Tweak the validation code, fix some overwriting when dumping automata

---
+++ src/main/scala/uuverifiers/common/ExperimentRunner.scala
@@ -0,0 +1,54 @@
+package uuverifiers.common
+
+import uuverifiers.catra.{CommandLineOptions, Instance, SatisfactionResult}
+import uuverifiers.catra.SolveRegisterAutomata.measureTime
+
+import java.util.concurrent.{ExecutorService, Executors}
+import scala.collection.MapView
+import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.concurrent.duration.Duration
+import scala.util.Try
+
+sealed class ExperimentRunner(
+    experiments: Seq[(String, Instance, CommandLineOptions, String)],
+    nrWorkers: Int
+) {
+
+  // Default: use half the CPUs
+  private val threadPool: ExecutorService =
+    Executors.newFixedThreadPool(nrWorkers)
+
+  implicit private val ec: ExecutionContext = new ExecutionContext {
+
+    def execute(runnable: Runnable): Unit = {
+      threadPool.submit(runnable)
+    }
+
+    def reportFailure(t: Throwable): Unit = {
+      println(s"WARN: error running experiment: $t")
+    }
+
+  }
+
+  private val tasks =
+    for ((instanceName, instance, config, configName) <- experiments)
+      yield Future {
+        val resultAndTime =
+          measureTime(config.getBackend().solveSatisfy(instance))
+        (instanceName, configName, resultAndTime)
+      }
+  def results(
+      deadline: Duration
+  ): Try[MapView[String, Seq[(String, (Try[SatisfactionResult], Double))]]] =
+    Try(
+      Await
+        .result(Future.sequence(tasks), deadline)
+        .groupBy(_._1)
+        .view
+        .mapValues(_.map(s => (s._2, s._3)))
+    )
+
+  def stop(): Unit = {
+    threadPool.shutdown()
+  }
+}
