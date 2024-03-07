6fca912a465128b36b5c6b107bfc73547df88044
Amanda Stjerna <amanda.stjerna@it.uu.se>
2024-03-07 21:26:18 +0100

    Add a straightforward experiment runner

--- oopsla-experiments.sh
+++ oopsla-experiments.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
-CATRA_TAG=".cactus" CATRA_TIMEOUT="120000" ./bin/experiments.sh ../parikh-plus-selected
-CATRA_TIMEOUT="30000" CATRA_CONFIGS="nuxmv,baseline,lazy" ./bin/experiments.sh ../deduped-benchmarks
+# 120s benchmarks:
+CATRA_TIMEOUT=120000\
+    CATRA_CONFIGS="nuxmv,lazy,lazy-no-clauselearning-no-restarts,lazy-eager-1,lazy-lazy-200" \
+    CATRA_THREADS=4 ./bin/experiments.sh 120s-experiments.d
+CATRA_TIMEOUT=120000 CATRA_CONFIGS=baseline CATRA_THREADS=2 ./bin/experiments.sh 120s-experiments.d
+
+# 30s benchmarks:
+CATRA_TIMEOUT=30000 CATRA_CONFIGS=lazy,nuxmv CATRA_THREADS=6 ./bin/experiments.sh deduped-benchmarks
+CATRA_TIMEOUT=30000 CATRA_CONFIGS=baseline CATRA_THREADS=2 ./bin/experiments.sh deduped-benchmarks
