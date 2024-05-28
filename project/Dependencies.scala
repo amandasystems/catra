845b6700f160bf901a3c9d935183265a2012134a
Philipp Ruemmer <ph_r@gmx.net>
2024-05-28 15:37:55 +0200

    updated versions

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.0"
   lazy val scalaCheck = "org.scalatestplus" %% "scalacheck-1-15" % "3.2.2.0"
-  lazy val princess = "io.github.uuverifiers" %% "princess" % "2024-01-12"
+  lazy val princess = "io.github.uuverifiers" %% "princess" % "2024-03-22"
   //lazy val princess = "uuverifiers" %% "princess-parser" % "nightly-SNAPSHOT"
-  lazy val fastparse = "com.lihaoyi" %% "fastparse" % "2.2.2"
+  lazy val fastparse = "com.lihaoyi" %% "fastparse" % "3.0.2"
 }
